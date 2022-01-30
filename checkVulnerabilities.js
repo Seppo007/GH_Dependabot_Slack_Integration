8a3098cddc83b34ebfac04b4f646ee7b90bb9d3c
Sebastian Schalow <schalowseb@gmail.com>
2022-01-30 21:12:21 +0100

    Reformat code and namings. Exit with status code 1 if some step of the script fails. Use exit code of script to propagate back to github actions

--- checkVulnerabilities.js
+++ checkVulnerabilities.js
@@ -1,6 +1,43 @@
 const https = require('https');
 const graphql = require('graphql-request');
 
+var owner = process.argv[2];
+var repo = process.argv[3];
+var githubAccessToken = process.argv[4];
+var slackWebhookEndpoint = process.argv[5];
+
+const severityRanking = {
+    LOW: 0,
+    MODERATE: 1,
+    HIGH: 2,
+    CRITICAL: 3
+};
+
+const severityColor = {
+    LOW: '#99c140',
+    MODERATE: '#e7b416',
+    HIGH: '#db7b2b',
+    CRITICAL: '#cc3232'
+}
+
+const sortBySeverity = (vulnerabilities) => vulnerabilities.sort(
+    (entryA, entryB) => severityRanking[entryB.severity] - severityRanking[entryA.severity]
+);
+
+const sortedVulnerabiliyEntriesFrom = (graphqlResponse) => {
+    const graphqlResponseEntries = graphqlResponse.repository.vulnerabilityAlerts.nodes;
+    const vulnerabilities = graphqlResponseEntries.map(entry => (
+        {
+            "severity": entry.securityVulnerability.severity,
+            "packageName": entry.securityVulnerability.package.name,
+            "vulnerableSince": entry.securityAdvisory.publishedAt,
+            "currentVersion": entry.vulnerableRequirements,
+            "patchedVersion": entry.securityVulnerability.firstPatchedVersion.identifier
+        }
+    ));
+    return sortBySeverity(vulnerabilities);
+}
+
 vulnerabilityReportFor = async (owner, repo, githubAccessToken) => {
     var endpoint = "https://api.github.com/graphql";
 
@@ -33,41 +70,8 @@ vulnerabilityReportFor = async (owner, repo, githubAccessToken) => {
     return sortedVulnerabiliyEntriesFrom(await graphQLClient.request(query));
 }
 
-const severityRanking = {
-    LOW: 0,
-    MODERATE: 1,
-    HIGH: 2,
-    CRITICAL: 3
-};
-
-const severityColor = {
-    LOW: '#99c140',
-    MODERATE: '#e7b416',
-    HIGH: '#db7b2b',
-    CRITICAL: '#cc3232'
-}
-
-const sortBySeverity = (vulnerabilities) => vulnerabilities.sort(
-    (entryA, entryB) => severityRanking[entryB.severity] - severityRanking[entryA.severity]
-);
-
-const sortedVulnerabiliyEntriesFrom = (response) => {
-    const responseEntries = response.repository.vulnerabilityAlerts.nodes;
-    const vulnerabilities = responseEntries.map(entry => (
-        {
-            "severity": entry.securityVulnerability.severity,
-            "packageName": entry.securityVulnerability.package.name,
-            "vulnerableSince": entry.securityAdvisory.publishedAt,
-            "currentVersion": entry.vulnerableRequirements,
-            "patchedVersion": entry.securityVulnerability.firstPatchedVersion.identifier
-        }
-    ));
-    return sortBySeverity(vulnerabilities);
-}
-
-const sendSlackMessage = (slackWebhookEndpoint, messageBody) => {
-
-    const slackAttachments = messageBody.map((vulnerability) => {
+const sendSlackMessage = (slackWebhookEndpoint, vulnerabilites) => {
+    const slackAttachments = vulnerabilites.map((vulnerability) => {
         return {
             fallback: vulnerability.packageName,
             text: vulnerability.packageName,
@@ -83,48 +87,40 @@ const sendSlackMessage = (slackWebhookEndpoint, messageBody) => {
     });
 
     const slackNotification = {
-        'username': 'Dependabot Vulnerability Reporter', // This will appear as user name who posts the message
-        'text': 'Found the following vulnerabilities in ' + process.env.github_repo + ':', // text
-        'icon_emoji': ':male-detective:', // User icon, you can also use custom icons here
+        'username': 'Dependabot Vulnerability Reporter',
+        'text': 'Found the following vulnerabilities in ' + process.env.github_repo + ':',
+        'icon_emoji': ':male-detective:',
         'attachments': slackAttachments,
     };
 
     const requestOptions = {
         method: 'POST',
-        header: {
-            'Content-Type': 'application/json'
-        }
+        header: { 'Content-Type': 'application/json' }
     };
 
     return new Promise((resolve, reject) => {
         const request = https.request(slackWebhookEndpoint, requestOptions, (res) => {
             let response = '';
-
-            res.on('data', (d) => {
-                response += d;
-            });
-
-            res.on('end', () => {
-                resolve(response);
-            })
+            res.on('data', (d) => response += d);
+            res.on('end', () => resolve(response));
         });
 
-        request.on('error', (e) => {
-            reject(e);
-        });
+        request.on('error', (e) => reject(e));
 
         request.write(JSON.stringify(slackNotification));
         request.end();
     });
 }
 
-var owner = process.argv[2];
-var repo = process.argv[3];
-var githubAccessToken = process.argv[4];
-var slackWebhookEndpoint = process.argv[5];
-
 vulnerabilityReportFor(owner, repo, githubAccessToken)
     .then((vulnerabilites) =>
         sendSlackMessage(slackWebhookEndpoint, vulnerabilites)
-            .then(() => console.log('succesfully send slack notification'), (e) => console.log('could not send slack message because of:', e)),
-        (e) => console.log("could not fetch vulnerabilities because of:", e));
+            .then(() =>
+                console.log('succesfully sent slack notification'), (e) => {
+                    console.log('could not send slack notification because of:', e);
+                    process.exit(1);
+                }),
+        (e) => {
+            console.log("could not fetch vulnerabilities because of:", e);
+            process.exit(1);
+        });
